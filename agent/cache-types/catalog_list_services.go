b71d3d3fa7be1d1b55c242e8e0cde7170a72d1e4
Daniel Nephin <dnephin@hashicorp.com>
2020-07-06 13:26:56 -0400

    agent/cache: Use AllowNotModifiedResponse in CatalogListServices
    Co-authored-by: Pierre Souchay <pierresouchay@users.noreply.github.com>

--- agent/cache-types/catalog_list_services.go
+++ agent/cache-types/catalog_list_services.go
@@ -38,15 +38,23 @@ func (c *CatalogListServices) Fetch(opts cache.FetchOptions, req cache.Request) 
 	// going to be served from cache and end up arbitrarily stale anyway. This
 	// allows cached service-discover to automatically read scale across all
 	// servers too.
-	reqReal.AllowStale = true
+	reqReal.QueryOptions.AllowStale = true
 
-	// Fetch
+	if opts.LastResult != nil {
+		reqReal.QueryOptions.AllowNotModifiedResponse = true
+	}
+
 	var reply structs.IndexedServices
 	if err := c.RPC.RPC("Catalog.ListServices", reqReal, &reply); err != nil {
 		return result, err
 	}
 
-	result.Value = &reply
 	result.Index = reply.QueryMeta.Index
+
+	if reply.QueryMeta.NotModified {
+		result.Value = opts.LastResult.Value
+		return result, nil
+	}
+	result.Value = &reply
 	return result, nil
 }
--- agent/cache-types/catalog_list_services_test.go
+++ agent/cache-types/catalog_list_services_test.go
@@ -60,3 +60,50 @@ func TestCatalogListServices_badReqType(t *testing.T) {
 	require.Contains(t, err.Error(), "wrong type")
 	rpc.AssertExpectations(t)
 }
+
+func TestCatalogListServices_NotModifiedResponse(t *testing.T) {
+	rpc := &MockRPC{}
+	typ := &CatalogListServices{RPC: rpc}
+
+	services := map[string][]string{
+		"foo": {"prod", "linux"},
+		"bar": {"qa", "windows"},
+	}
+	rpc.On("RPC", "Catalog.ListServices", mock.Anything, mock.Anything).
+		Return(nil).
+		Run(func(args mock.Arguments) {
+			req := args.Get(1).(*structs.DCSpecificRequest)
+			require.Equal(t, uint64(44), req.QueryOptions.MinQueryIndex)
+			require.Equal(t, 1*time.Second, req.QueryOptions.MaxQueryTime)
+			require.True(t, req.AllowStale)
+			require.True(t, req.AllowNotModifiedResponse)
+
+			reply := args.Get(2).(*structs.IndexedServices)
+			reply.QueryMeta.Index = 44
+			reply.NotModified = true
+		})
+
+	opts := cache.FetchOptions{
+		MinIndex: 44,
+		Timeout:  time.Second,
+		LastResult: &cache.FetchResult{
+			Value: &structs.IndexedServices{
+				Services:  services,
+				QueryMeta: structs.QueryMeta{Index: 44},
+			},
+			Index: 44,
+		},
+	}
+	actual, err := typ.Fetch(opts, &structs.DCSpecificRequest{Datacenter: "dc1"})
+	require.NoError(t, err)
+	expected := cache.FetchResult{
+		Value: &structs.IndexedServices{
+			Services:  services,
+			QueryMeta: structs.QueryMeta{Index: 44},
+		},
+		Index: 44,
+	}
+	require.Equal(t, expected, actual)
+
+	rpc.AssertExpectations(t)
+}
